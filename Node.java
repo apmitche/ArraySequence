360f8a6bbe658dae354434ed2b7be84deeda9aeb
Alicia Mitchell <apmitche@mtu.edu>
2016-01-30 09:59:16 -0600

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,39 @@
+package cs2321;
+import net.datastructures.Position;
+
+public class Node<E> implements Position<E>{
+
+	E element;
+	int index;
+	
+	public Node(int newIndex, E newElement){
+		
+		element = newElement;
+		index = newIndex;		
+	}
+
+	public E getElement() {
+		return element;
+	}
+
+	public void setElement(E element) {
+		this.element = element;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	@Override
+	public E element() {
+
+		return element;
+	}
+	
+}
+
+
